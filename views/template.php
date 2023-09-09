f366c99286972707093322b0fe6b4132e3adc44e
Clément Bartholomé <clementoss@gmail.com>
2023-09-09 23:04:33 +0200

    style links

--- views/template.php
+++ views/template.php
@@ -11,11 +11,11 @@
             <section class="flex p-8 items-center justify-between max-w-5xl m-auto">
                 <a href="index.php" class="text-3xl">Shopie<span class="text-blue-500">.</span></a>
                 <nav class="text-xl">
-                    <a class="mx-6" href="index.php">Accueil</a>
-                    <a class="mx-6" href="index.php?action=about">À propos</a>
-                    <a class="mx-6" href="index.php?action=parcourir">Parcourir</a>
-                    <a class="mx-6" href="index.php?action=contact">Contact</a>
-                    <a class="mx-6" href="index.php?action=admin">Admin</a>
+                    <a class="mx-6 hover:text-cyan-600 hover:underline" href="index.php">Accueil</a>
+                    <a class="mx-6 hover:text-cyan-600 hover:underline" href="index.php?action=about">À propos</a>
+                    <a class="mx-6 hover:text-cyan-600 hover:underline" href="index.php?action=parcourir">Parcourir</a>
+                    <a class="mx-6 hover:text-cyan-600 hover:underline" href="index.php?action=contact">Contact</a>
+                    <a class="mx-6 hover:text-cyan-600 hover:underline" href="index.php?action=admin">Admin</a>
                 </nav>
                 <div>
                 <a href="search_page.php"><i class="fas fa-search text-2xl ml-2"></i></a>
@@ -41,34 +41,34 @@
 
                 <div class="flex flex-col gap-4">
                     <h3 class="capitalize text-xl font-bold">liens rapides</h3>
-                    <a href="home.php" class="text-zinc-500"> <i class="fas fa-angle-right pr-2 text-cyan-600	"></i> accueil</a>
-                    <a href="about.php" class="text-zinc-500"> <i class="fas fa-angle-right pr-2 text-cyan-600	"></i> à propos</a>
-                    <a href="shop.php" class="text-zinc-500"> <i class="fas fa-angle-right pr-2 text-cyan-600	"></i> parcourir</a>
-                    <a href="contact.php" class="text-zinc-500"> <i class="fas fa-angle-right pr-2 text-cyan-600	"></i> contact</a>
+                    <a href="home.php" class="group text-zinc-500 hover:text-cyan-600"> <i class="fas fa-angle-right pr-2 text-cyan-600 duration-100 group-hover:pr-4"></i> accueil</a>
+                    <a href="about.php" class="group text-zinc-500 hover:text-cyan-600"> <i class="fas fa-angle-right pr-2 text-cyan-600 duration-100 group-hover:pr-4"></i> à propos</a>
+                    <a href="shop.php" class="group text-zinc-500 hover:text-cyan-600"> <i class="fas fa-angle-right pr-2 text-cyan-600 duration-100 group-hover:pr-4"></i> parcourir</a>
+                    <a href="contact.php" class="group text-zinc-500 hover:text-cyan-600"> <i class="fas fa-angle-right pr-2 text-cyan-600 duration-100 group-hover:pr-4"></i> contact</a>
                 </div>
 
                 <div class="flex flex-col gap-4">
                     <h3 class="capitalize text-xl font-bold">liens supplémentaires</h3>
-                    <a href="user_login.php" class="text-zinc-500"> <i class="fas fa-angle-right pr-2 text-cyan-600	"></i> connexion</a>
-                    <a href="user_register.php" class="text-zinc-500"> <i class="fas fa-angle-right pr-2 text-cyan-600	"></i> inscription</a>
-                    <a href="cart.php" class="text-zinc-500"> <i class="fas fa-angle-right pr-2 text-cyan-600	"></i> panier</a>
-                    <a href="orders.php" class="text-zinc-500"> <i class="fas fa-angle-right pr-2 text-cyan-600	"></i> commandes</a>
+                    <a href="user_login.php" class="group text-zinc-500 hover:text-cyan-600"> <i class="fas fa-angle-right pr-2 text-cyan-600 duration-100 group-hover:pr-4"></i> connexion</a>
+                    <a href="user_register.php" class="group text-zinc-500 hover:text-cyan-600"> <i class="fas fa-angle-right pr-2 text-cyan-600 duration-100 group-hover:pr-4"></i> inscription</a>
+                    <a href="cart.php" class="group text-zinc-500 hover:text-cyan-600"> <i class="fas fa-angle-right pr-2 text-cyan-600 duration-100 group-hover:pr-4"></i> panier</a>
+                    <a href="orders.php" class="group text-zinc-500 hover:text-cyan-600"> <i class="fas fa-angle-right pr-2 text-cyan-600 duration-100 group-hover:pr-4"></i> commandes</a>
                 </div>
 
                 <div class="flex flex-col gap-4">
                     <h3 class="capitalize text-xl font-bold">contactez-nous</h3>
-                    <a href="tel:1234567890" class="text-zinc-500"><i class="fas fa-phone pr-2 text-cyan-600	"></i> +123 456 7899</a>
-                    <a href="tel:11122233333" class="text-zinc-500"><i class="fas fa-phone pr-2 text-cyan-600	"></i> +111 222 3333</a>
-                    <a href="mailto:shopie@gmail.com" class="text-zinc-500"><i class="fas fa-envelope pr-2 text-cyan-600	"></i> shopie@gmail.com</a>
-                    <a href="https://www.google.com/myplace" class="text-zinc-500"><i class="fas fa-map-marker-alt pr-2 text-cyan-600	"></i> paris, france - 75001 </a>
+                    <a href="tel:1234567890" class="group text-zinc-500 hover:text-cyan-600"> <i class="fas fa-angle-right pr-2 text-cyan-600 duration-100 group-hover:pr-4"></i> +123 456 7899</a>
+                    <a href="tel:11122233333" class="group text-zinc-500 hover:text-cyan-600"> <i class="fas fa-angle-right pr-2 text-cyan-600 duration-100 group-hover:pr-4"></i> +111 222 3333</a>
+                    <a href="mailto:shopie@gmail.com" class="group text-zinc-500 hover:text-cyan-600"> <i class="fas fa-angle-right pr-2 text-cyan-600 duration-100 group-hover:pr-4"></i> shopie@gmail.com</a>
+                    <a href="https://www.google.com/myplace" class="group text-zinc-500 hover:text-cyan-600"> <i class="fas fa-angle-right pr-2 text-cyan-600 duration-100 group-hover:pr-4"></i> paris, france - 75001 </a>
                 </div>
 
                 <div class="flex flex-col gap-4">
                     <h3 class="capitalize text-xl font-bold">suivez-nous</h3>
-                    <a href="#" class="text-zinc-500"><i class="fab fa-facebook-f pr-2 text-cyan-600	"></i>facebook</a>
-                    <a href="#" class="text-zinc-500"><i class="fab fa-twitter pr-2 text-cyan-600	"></i>twitter</a>
-                    <a href="#" class="text-zinc-500"><i class="fab fa-instagram pr-2 text-cyan-600	"></i>instagram</a>
-                    <a href="#" class="text-zinc-500"><i class="fab fa-linkedin pr-2 text-cyan-600	"></i>linkedin</a>
+                    <a href="#" class="group text-zinc-500 hover:text-cyan-600"> <i class="fas fa-angle-right pr-2 text-cyan-600 duration-100 group-hover:pr-4"></i>facebook</a>
+                    <a href="#" class="group text-zinc-500 hover:text-cyan-600"> <i class="fas fa-angle-right pr-2 text-cyan-600 duration-100 group-hover:pr-4"></i>twitter</a>
+                    <a href="#" class="group text-zinc-500 hover:text-cyan-600"> <i class="fas fa-angle-right pr-2 text-cyan-600 duration-100 group-hover:pr-4"></i>instagram</a>
+                    <a href="#" class="group text-zinc-500 hover:text-cyan-600"> <i class="fas fa-angle-right pr-2 text-cyan-600 duration-100 group-hover:pr-4"></i>linkedin</a>
                 </div>
 
             </section>
